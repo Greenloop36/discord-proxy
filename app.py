5ffc71617198a756e7e588ebf7a910433b743ef2
gl36 <ewanakira@gmail.com>
2025-07-28 12:28:41 +0100

    Build app

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+"""
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+    Discord API Proxy
+    28/07/2025
+
+"""
+
+
+## Imports
+from rich import print
+from rich.console import Console
+
+from flask import Flask, request
+from markupsafe import escape
+import requests
+
+## API
+app = Flask(__name__)
+console = Console()
+
+@app.route('/api/webhooks/<string:webhook_id>/<string:webhook_token>', methods=['POST'])
+def webhook_proxy(id: str, token: str):
+    url: str = f"https://discord.com/api/webhooks/{id}/{token}"
+    body: dict = request.get_json()
+
+    try:
+        response = requests.post(url, body)
+        response.raise_for_status()
+    except requests.HTTPError:
+        return response.text, response.status_code
+    except Exception as e:
+        console.print_exception(show_locals=True)
+        return "Internal Error", 500
+    else:
+        return "OK", 200
 
 
 if __name__ == "__main__":
